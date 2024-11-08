78f09c3cb894693bd2daf46ca84c1cf3f82cf336
songle <951540966@qq.com>
2024-11-08 12:29:53 +0800

    fix: ui

--- src/views/test/SvgMaskDemo.tsx
+++ src/views/test/SvgMaskDemo.tsx
@@ -1,7 +1,7 @@
 export function SvgMaskDemo() {
 	return (
 		<div className="page-svg-mask-demo bg-[#e5e6eb] min-h-[100vh]">
-			<svg width="800" height="800" className="bg-[#00B42A]">
+			<svg width="800" height="800" className="bg-[#165DFF]">
 				<image href="/demo-parking.jpg" width="700" />
 				<mask id="myMask">
 					{/* 白色像素下的所有内容都将可见 */}
@@ -9,7 +9,7 @@ export function SvgMaskDemo() {
 					{/* 黑色像素下的所有内容都将不可见 */}
 					<rect x="200" y="200" width="200" height="100" fill="black" />
 				</mask>
-				<rect x="0" y="0" width="800" height="800" fill="#165DFF" opacity={0.5} mask="url(#myMask)" />
+				<rect x="0" y="0" width="800" height="800" fill="black" opacity={0.4} mask="url(#myMask)" />
 				<rect x="200" y="200" width="200" height="100" fill="transparent" stroke="yellow" />
 			</svg>
 
