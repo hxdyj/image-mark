e49f503f4f00ed81d547c7114f884e44237aef3c
songle <951540966@qq.com>
2025-01-07 14:59:58 +0800

    fix: package #/

--- package/action/index.ts
+++ package/action/index.ts
@@ -2,7 +2,7 @@ import { Shape } from "@svgdotjs/svg.js";
 import { EventBindingThis } from "../event";
 import { ImageMarkShape } from "../shape/Shape";
 import ImageMark from "..";
-import { ShapePlugin } from "#/plugins/ShapePlugin";
+import { ShapePlugin } from "../plugins/ShapePlugin";
 
 export class Action extends EventBindingThis {
 	static actionName: string;
