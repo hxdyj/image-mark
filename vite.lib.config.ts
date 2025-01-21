affece475e89e4324c0edeb2da17b3b17ce151e8
songle <951540966@qq.com>
2025-01-21 09:55:01 +0800

    fix: lib config

--- vite.lib.config.ts
+++ vite.lib.config.ts
@@ -2,12 +2,12 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 import strip from '@rollup/plugin-strip'
-
+import pkg from './package.json'
 export default defineConfig({
 	build: {
 		lib: {
 			entry: path.resolve(__dirname, './package/index.ts'),
-			name: 'image-mark',
+			name: pkg.name,
 			fileName: format => `index.${format}.js`,
 		},
 		minify: false
